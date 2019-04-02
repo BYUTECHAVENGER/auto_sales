1f49acf98586a6a3f57d371199fc0597b0a5002a
Paulie <sny17007@byui.edu>
2019-04-01 22:38:32 -0400

    added images and recreated vehicle table

--- tables.sql
+++ tables.sql
@@ -6,7 +6,8 @@ color VARCHAR(255),
 manu_year INT,
 class VARCHAR(255),
 doors INT,
-price INT
+price INT,
+img VARCHAR(255)
 );
 
 CREATE TABLE users(
@@ -19,12 +20,12 @@ passwd VARCHAR(255)
 );
 
 
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Dodge', 'Dart','Orange', 2019, 'Coupe', 2, 22100);
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Dodge', 'Challenger','Black', 2020, 'Coupe', 2, 41000);
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Dodge', 'Ram 1500','Blue', 2004, 'P/U', 2, 2100);
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Volvo', '240','Beige', 1990, 'Sedan', 4, 1900);
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Fiat', '500','Red', 2004, 'Coupe', 2, 3100);
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Honda', 'Civic','Purple', 2010, 'Sedan', 4, 3800);
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Chevy', 'S10','Black', 2001, 'P/U', 2, 1700);
-INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price) VALUES('Ford', 'Fiesta','Red', 1999, 'coupe', 2, 850);
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Dodge', 'Dart','Orange', 2019, 'Coupe', 2, 22100,'2019-Dart');
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Dodge', 'Challenger','Black', 2020, 'Coupe', 2, 41000, '2020_challenger');
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Dodge', 'Ram 1500','Blue', 2004, 'P/U', 2, 2100, '2004_Ram1500');
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Volvo', '240','Beige', 1990, 'Sedan', 4, 1900, '1990_Volvo_240');
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Fiat', '500','Red', 2004, 'Coupe', 2, 3100, '2004_FIAT_500');
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Honda', 'Civic','Purple', 2010, 'Sedan', 4, 3800, '2010_Honda_Civic');
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Chevy', 'S10','Black', 2001, 'P/U', 2, 1700, '2001_Chevy_S10');
+INSERT INTO Vehicles (make, model, color, manu_year, class, doors, price, img) VALUES('Ford', 'Fiesta','Red', 1999, 'coupe', 2, 850, '1999_Ford_Fiesta');
 
