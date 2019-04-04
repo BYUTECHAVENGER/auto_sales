0b61d624c1761335b3666a7ad697300a3fb4c8fe
Paulie <sny17007@byui.edu>
2019-04-04 17:09:13 -0400

    console.log (newCar) ln24

--- public/FindCarResult.js
+++ public/FindCarResult.js
@@ -20,6 +20,8 @@ function findCar() {
             newCar += result.carChoice[i].make + "<br>" + result.carChoice[i].model
             + "<br>" +  result.carChoice[i].color + "<img src='" + result.carChoice[i].img + ".jpg'>"
            + "<hr>"
+
+           console.log (newCar)
         }
         $('#newCar').html(newCar);
     })
