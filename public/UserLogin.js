ab0da5284425b117870c6f5472aa81002e456bbf
Paulie <sny17007@byui.edu>
2019-04-08 18:24:50 -0400

    successfulLogin adjust

--- public/UserLogin.js
+++ public/UserLogin.js
@@ -11,7 +11,7 @@ function login() {
     $.post("/UserLogin", query, function (result) {
         console.log(result);
 
-        if(result.successfullLogin == true) {
+        if(result.successfulLogin == true) {
             window.location.replace('Add_Auto_For_Sale.html')
         }
 
